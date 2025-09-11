9ee21864cb1862f6c69ed59cc2cc22ed1eb071b3
Qasem Ehab kamel Aboalia <qassimalia77@gmail.com>
2025-09-11 21:55:33 +0300

    Add files via upload

---
+++ third_project_dojo_survey/third_project_dojo_survey/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for third_project_dojo_survey project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'third_project_dojo_survey.settings')
+
+application = get_asgi_application()
