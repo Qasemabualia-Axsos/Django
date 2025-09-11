355a29e0094cc3e53d8ba0ca1d68ea5b47a32390
Qasem Ehab kamel Aboalia <qassimalia77@gmail.com>
2025-09-11 22:00:08 +0300

    Add files via upload

---
+++ Users_with_Templates/Users_with_Templates/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Users_with_Templates project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Users_with_Templates.settings')
+
+application = get_asgi_application()
